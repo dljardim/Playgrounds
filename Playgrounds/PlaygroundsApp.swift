60374d5af74d429c6dd2840b22ff549751038de8
dljardim <dljardim@outlook.com>
2025-02-17 10:25:06 -0600

    Initial Commit

---
+++ Playgrounds/PlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundsApp.swift
+//  Playgrounds
+//
+//  Created by Damian Jardim on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
